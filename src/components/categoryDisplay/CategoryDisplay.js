ea7f84f4cec6e82225d1c508a6e598ba0d0e76b5
Daniel Bucket <danieljbucket@gmail.com>
2017-05-09 09:05:12 -0600

    some to fix

--- src/components/categoryDisplay/CategoryDisplay.js
+++ src/components/categoryDisplay/CategoryDisplay.js
@@ -1,35 +1,10 @@
 import React from 'react';
 import style from './categoryDisplay.css'
 
-import People from '../Person/Person'
-
-import Planets from '../Planets/Planets'
-
-import Vehicle from '../Vehicle/Vehicle'
-
 const CategoryDisplay = ({ presentCategory, typeCategory, selectedFavorites }) => {
-  let list;
-  switch(typeCategory){
-    case "people":
-     list = presentCategory.map((data,i)=>{
-       return <People key ={i} data={data}/>
-     })
-     break;
 
-    case 'planets':
-    list = presentCategory.map((data,i)=>{
-      return <Planets key={i} data={data}/>
-    })
-    break;
-
-
-    case 'vehicles':
-    list = presentCategory.map((data,i) =>{
-      return <Vehicle key={i} data={data}/>
-    })
-    break;
-  }
-
+  // not recieving any props...?
+  console.log('looking for props :', presentCategory);
 
   // if (typeCategory === "people") {
   //   list =
@@ -38,9 +13,8 @@ const CategoryDisplay = ({ presentCategory, typeCategory, selectedFavorites }) =
   //
 
   return (
-    <div>
-    {list}
-
+    <div className="category-display">
+      { presentCategory }some shtuff about Darth Skywalker and his posse
     </div>
   )
 }
