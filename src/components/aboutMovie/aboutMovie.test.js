954327fdc1c4ca4b9d91ea7331a48f6a0a516c43
Chris Jordan <cjorda15@msudenver.edu>
2017-05-09 20:00:27 -0600

    added some test, more to come

---
+++ src/components/aboutMovie/aboutMovie.test.js
@@ -0,0 +1,35 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import AboutMovie from './AboutMovie';
+import {mount,shallow} from 'enzyme'
+
+describe('aboutMovie test',()=>{
+
+  it('should not crash when rendered',()=>{
+      const div = document.createElement('div');
+      ReactDOM.render(<AboutMovie
+        movieSummary={["s","t","a","r"]}
+        />, div);
+  })
+
+  it('should have a class',()=>{
+    const wrapper = mount(<AboutMovie movieSummary={["s","t","a","r"]}/>)
+    expect(wrapper.hasClass('about-movie-container')).toEqual(true)
+  })
+
+  it('contains elements',()=>{
+    const wrapper = mount(<AboutMovie movieSummary={["s","t","a","r"]}/>)
+    expect(wrapper.find('div').length).toEqual(3)
+    expect(wrapper.find('button').length).toEqual(1)
+    expect(wrapper.find('h4').length).toEqual(2)
+    expect(wrapper.find('p').length).toEqual(1)
+  })
+
+  it('should have a button that can be clicked',()=>{
+    const wrapper = mount(<AboutMovie movieSummary={["s","t","a","r"]}/>)
+    const btn = wrapper.find('button')
+
+  })
+
+
+})
