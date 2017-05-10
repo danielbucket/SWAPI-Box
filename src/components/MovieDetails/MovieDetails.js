6f853d4d6c3b6cb2eadef6370472ff0990f9f1b5
Chris Jordan <cjorda15@msudenver.edu>
2017-05-10 10:53:39 -0600

    added movie info that can be shown when clicking show more movie info

---
+++ src/components/MovieDetails/MovieDetails.js
@@ -0,0 +1,29 @@
+import React from 'react'
+import './MovieDetails.css'
+
+const MovieDetails = ({movieSummary})=>{
+  return(
+    <div className = "movie-summary">
+      <div className = "movie-title">
+      {movieSummary.title}
+      </div>
+      <div className = "movie-info">
+      <span className = "movie-cat">Director:</span>{movieSummary.director}
+      </div>
+      <div className = "movie-info">
+      <span className = "movie-cat">Producer:</span>{movieSummary.producer}
+      </div>
+      <div className = "movie-info">
+      <span className = "movie-cat">ReleaseDate:</span>{movieSummary.release_date}
+      </div>
+      <div className = "movie-info">
+      <span className = "movie-cat">Movie-Info:</span>{movieSummary.opening_crawl}
+      </div>
+    </div>
+  )
+
+}
+
+
+
+export default MovieDetails
