58aea405b4cde7325b1a1aee98fd7bea6a189f53
Daniel Bucket <danieljbucket@gmail.com>
2017-05-10 16:27:51 -0600

    testing

--- src/App.test.js
+++ src/App.test.js
@@ -1,10 +1,11 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
-import {mount,shallow} from 'enzyme'
-import fetchMock from 'fetch-mock'
+import {mount,shallow} from 'enzyme';
+import fetchMock from 'fetch-mock';
+import mockMovieData from './helperApi/MockMovieData'
 
-describe('App test',()=>{
+describe('App test', () => {
 
   it('renders without crashing', () => {
     const div = document.createElement('div');
@@ -36,8 +37,37 @@ describe('App test',()=>{
     expect(wrapper.state()).toEqual(expected)
   })
 
-  it('can change its state',()=>{
-    const wrapper = mount(<App/>)
+  it.only('can change its state', async () =>  {
+
+    beforeEach( () => {
+      fetchMock.get(mockMovieData, {
+        status: 200,
+        body: mockMovieData
+      })
+    })
+
+    const wrapper = mount(<App />)
+    const button = wrapper.find('.learn-more')
+    console.log(button);
+
+    const waitItOut = new Promise( (resolve, reject) => {
+      setTimeout( () => {
+        resolve('success')
+      }, 2000)
+    })
+
+    expect(wrapper.state().defaultAboutMovie.title).toEqual(undefined)
+
+    await waitItOut;
+    wrapper.update()
+
+    expect(typeof wrapper.state().defaultAboutMovie.title).toEqual("string")
+    expect(typeof wrapper.state().defaultAboutMovie.opening_crawl).toEqual("string")
+
+    expect(typeof wrapper.state().aboutMovie).toEqual("string")
+    button.simulate('click')
+    expect(typeof wrapper.state().aboutMovie).toEqual()
+
 
 
   })
